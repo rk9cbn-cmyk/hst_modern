3a6e7f6dea9c51ed300e3ce749ed3c20e9a4046d
rk9cbn-cmyk <rk9cbn@gmail.com>
2026-03-31 17:10:18 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'hst-modern-v1';
+const ASSETS = [
+  'hst_modern.html',
+  'manifest.json',
+  'icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
